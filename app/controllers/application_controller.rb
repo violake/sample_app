a4174aded789f4574ec938c01ee8193b63b18726
violake <violake@foxmail.com>
2017-06-27 17:36:24 +1000

    bug fix2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   def hello
-    render html "hello roger!"
+    render html: "hello roger!"
   end
 end
